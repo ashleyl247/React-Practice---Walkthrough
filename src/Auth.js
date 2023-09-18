747f2acd94f9472392955c837970e99b4e57549b
Ashley Lewis <ashley.l247work@gmail.com>
2023-09-18 10:14:08 +0700

    coding halfway point

---
+++ src/Auth.js
@@ -0,0 +1,37 @@
+import React from "react"
+
+export default function (props) {
+    return (
+        <div className="Auth-form-container">
+            <form className="Auth-form">
+                <div className="Auth-form-content">
+                    <h3 className="Auth-form-title">Sign In</h3>
+                    <div className="form-group mt-3">
+                        <label>Email Address</label>
+                        <input
+                            type="email"
+                            className="form-control mt-1"
+                            placeholder="Enter Email"
+                        />
+                    </div>
+                    <div className="form-group mt-3">
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            className="form-control mt-1"
+                            placeholder="Enter Password"
+                        />
+                    </div>
+                    <div className="d-grid gap-2 mt-3">
+                        <button type="submit" className="btn btn-primary">
+                            Submit
+                        </button>
+                    </div>
+                    <p className="forgot-password text-right mt-2">
+                        forgot <a href="#">password?</a>
+                    </p>
+                </div> 
+            </form>
+        </div>
+    )
+}
